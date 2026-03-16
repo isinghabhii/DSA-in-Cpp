5d0ca629d1119372a7688764507f0001a4b9404b
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-03-16 17:09:16 +0530

    Practice question

---
+++ Conditional Statements/odd_even.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout << "Enter num : ";
+    cin >> num;
+
+    if(num % 2 == 0){
+        cout << num << " is even number"<< endl;
+    } else {
+        cout << num << " is odd number" << endl;
+    }
+}
---
+++ Conditional Statements/pq.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout << "enter a : ";
+    cin >> a;
+    cout << "enter b : ";
+    cin >> b;
+
+    if(a >= b){
+        cout << "a is larger : " << a << endl;
+    } else {
+        cout << "b is larger : " << b << endl;
+    }
+}
