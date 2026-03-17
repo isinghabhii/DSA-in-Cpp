9064f1144bb5576a07e6108b521f4e6e51dc0926
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-03-17 17:19:46 +0530

    for loop

---
+++ Loops/for_loop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num;
+    for(num=1; num<=5; num++){
+        cout << num << " ";
+    }
+
+    cout << endl;
+    cout << "last value of num = " << num << endl;
+    return 0;
+}
+
+// iterate (run)
+// iteration (single run)
+// iterator (loop variable)
---
+++ Loops/sumOfNaturalNum.cpp
@@ -0,0 +1,18 @@
+// Print sum of first N natural numbers
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "enter your n : ";
+    cin >> n;
+
+    int sum = 0;
+    for(int i=1; i<=n; i++){
+        sum += i;
+    }
+    
+    cout <<"Sum = " << sum << endl;
+    return 0;
+}
