94073a723868b7d7aace992869537d877d17df14
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-03-18 23:49:44 +0530

    update prime num code

--- Loops/primeNum.cpp
+++ Loops/primeNum.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
     int n = 7;
     bool isPrime = true;
     
-    for(int i=2 ; i<=n-1; i++) {
+    // for(int i=2 ; i<=n-1; i++) {
+    for(int i=2 ; i<=sqrt(n); i++) {
         if(n % i == 0){ // i is a factor of n; i completely divides n; n is non-prime
             isPrime = false;
             break;
