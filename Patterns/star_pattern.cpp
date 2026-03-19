db3f3f996df030e7b2c120ec19884a8876736924
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-03-19 18:31:55 +0530

    code update

--- Patterns/star_pattern.cpp
+++ Patterns/star_pattern.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main(){
-    for(int i=1; i<=4; i++){
+    int n = 4;
+    for(int i=1; i<=n; i++){
         for(int j=1; j<=i; j++){
             cout << "*" << " ";
         }
