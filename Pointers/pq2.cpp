85ddec7a2a2981b210f4b321fc3b7e7b1b17d69a
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-03-23 10:15:05 +0530

    Practice question

---
+++ Pointers/pq2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float* a, b;
+    // a-> float ptr
+    // b -> normal float variable
+
+    int *ptr = 0; // yes, I can initialize a pointer with value 0.
+    cout << ptr << "\n";
+
+    return 0;
+}
