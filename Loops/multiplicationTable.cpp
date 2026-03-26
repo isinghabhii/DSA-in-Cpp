5ea78b0dd441ca30781aa6dba72f698dd91c512c
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-03-26 20:03:44 +0530

    code

---
+++ Loops/multiplicationTable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = 8;
+
+    for(int i=1; i<=10; i++){
+        cout << ( n * i) << " ";
+    }
+    
+    return 0;
+}
---
+++ Loops/primeNum2toN.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N = 15;
+    for(int i=2; i<=N; i++){
+        int curr = i;
+        bool isPrime = true;
+        for(int j=2; j*j<=i; j++){
+            if(curr % j == 0){
+                isPrime = false;
+            }
+        }
+
+        if(isPrime){
+            cout << curr << " ";
+        }
+    }
+    cout << "\n";
+}
