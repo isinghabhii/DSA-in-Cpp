f773301d99cbfb90c6fb3f3fd6113b29dc527ae9
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-03-30 09:35:29 +0530

    0-1 Triangle Pattern code

---
+++ Patterns/triangle_pattern.cpp
@@ -0,0 +1,17 @@
+// 0-1 Triangle Pattern
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    bool val = true;
+    int n = 5;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            cout << val << " ";
+            val = !val;
+        }
+        cout << "\n";
+    }
+}
