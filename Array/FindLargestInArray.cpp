7829480df1fe51381fc1b5fe404378e6822d3616
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-04-03 16:02:10 +0530

    Find Largest & Smallest Num in Array

---
+++ Array/FindLargestInArray.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {5, 4, 3, 9, 12};
+    int n = sizeof(arr) / sizeof(int);
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for(int i=0; i<n; i++){
+        // maximum
+        if(arr[i] > max){
+            max = arr[i];
+            // cout << "assigning val " << arr[i] << " to max\n";
+        }
+
+        //minimum
+        if(arr[i] < max){
+            min = arr[i];
+        }
+    }
+
+    cout << "Largest = " << max << endl;
+    cout << "Smallest = " << min << endl;
+    return 0;
+}
