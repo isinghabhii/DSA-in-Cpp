e4d5a98ef04f49de1de515500f61f2487140d842
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-04-03 16:01:37 +0530

    Array

---
+++ Array/array.cpp
@@ -0,0 +1,18 @@
+// Array => Linear collection of same type of elements that are stored together in contiguous memory spaces.
+// Array's index(position) start from 0
+
+// Creating an array => Memory is statically allocated (at compile time)
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int marks[5]= {1, 2, 3, 4, 5}; // 0 to 4
+    cout << marks[0] << endl; //1
+    cout << marks[1] << endl; //2
+    cout << marks[2] << endl; //3
+
+    cout << sizeof(marks) << endl; // 20B => 5 * 4B
+    cout << sizeof(marks)/sizeof(int) << endl; // 5 => length of array
+    return 0;
+}
