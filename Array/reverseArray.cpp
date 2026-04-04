7290a07430bc0d8a6970d8e30aa0ca747403ca23
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-04-04 16:33:53 +0530

    Reverse Array with extra space

---
+++ Array/reverseArray.cpp
@@ -0,0 +1,33 @@
+// with extra space
+
+#include<iostream>
+using namespace std;
+
+void printArr(int *arr, int n){
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    int arr[] = {5, 4, 3, 9, 2};
+    int n = sizeof(arr) / sizeof(int);
+
+    int copyArr[n]; // extra space
+
+    for(int i=0; i<n; i++){
+        int j = n-i-1;
+        copyArr[i] = arr[j];
+    }
+
+    for(int i=0; i<n; i++){
+        arr[i] = copyArr[i];
+    }
+
+    printArr(arr, n);
+    return 0;
+}
+
+// SC = O(n)
+// TC = O(n+n) = O(2n) => O(n)
