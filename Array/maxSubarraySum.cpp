3ec84eb17ea733c9bb2677fc1d4e539f9f2b011e
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-04-07 12:58:08 +0530

    maxSubarraySum using kadane's Algorithm

--- Array/maxSubarraySum.cpp
+++ Array/maxSubarraySum.cpp
@@ -35,11 +35,26 @@ void maxSubarraySum2(int *arr, int n){
     cout << "maximum subarray sum = " << maxSum << endl;
 }
 
+//3. Kadane's Algorithm => TC: O(n)
+void maxSubarraySum3(int *arr, int n){
+    int maxSum = INT8_MIN;
+    int currSum = 0;
+
+    for(int i=0; i<n; i++){
+        currSum += arr[i];
+        maxSum = max(currSum, maxSum);
+        if(currSum < 0){
+            currSum = 0;
+        }
+    }
+    cout << "maximum subarray sum = " << maxSum << endl;
+}
+
 
 int main(){
     int arr[6] = {2, -3, 6, -5, 4, 2};
     int n = sizeof(arr) / sizeof(int);
 
-    maxSubarraySum2(arr, n);
+    maxSubarraySum3(arr, n);
     return 0;
 }
