790eae5b80cbcc8b30edf4b6e64a56f91d1e8d62
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-04-11 16:59:52 +0530

    inbuilt sort

---
+++ Basic Sorting Algorithms/inbuiltSort.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <algorithm>
+using namespace std;
+
+void print(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
+int main(){
+    int arr[8] = {1, 4, 1, 3, 2, 4, 3, 7};
+    // sort(arr, arr+8); //ascending order
+    // sort(arr+2, arr+5);
+
+    sort(arr, arr+8, greater<int>()); //descending order
+
+    print(arr, 8);
+    return 0;
+}
