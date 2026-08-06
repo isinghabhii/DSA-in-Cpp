8435a64312412dd594e231f840a2d1721e2a61ae
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-08-06 10:36:54 +0530

    char array

---
+++ Char Array & String/char.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // char ch = 'a';
+    // cout << (int) ch << endl;
+
+    char ch = 'f';
+    int pos = ch - 'a';
+    cout << pos << endl;
+    
+    return 0;
+}
