68a6c4176d9123b0788896c3837e7427e3d82d2a
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-08-06 10:43:55 +0530

    char arr (cin.getline)

---
+++ Char Array & String/character_Array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+    // char word[30];
+    // cin >> word; // ignore whitespace 
+
+    // cout << "your word was: " << word << endl;
+    // cout << "length : " << strlen(word) << endl;
+
+
+    char sentence[30];
+    cin.getline(sentence, 30, '*'); 
+
+    cout << "your word was: " << sentence << endl;
+    cout << "length : " << strlen(sentence) << endl;
+    return 0;
+}
