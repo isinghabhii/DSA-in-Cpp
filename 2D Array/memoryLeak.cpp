4be75b0d6cfe4d9f472dfbc6d072bc591781eefc
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-08-07 16:03:51 +0530

    Memory leak

---
+++ 2D Array/memoryLeak.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int* func(){
+    int *ptr = new int;
+    *ptr = 1200;
+    cout << "ptr points to " << *ptr << endl;
+
+    return ptr;
+}
+
+int main(){
+    int *x = func();
+    cout << *x << endl;
+
+    return 0;
+}
