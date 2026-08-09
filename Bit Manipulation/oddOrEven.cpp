8497c16af74722a79868af946f9a94f4f09c2b1b
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-08-09 17:07:44 +0530

    oddOrEven using bitwise &

---
+++ Bit Manipulation/oddOrEven.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void oddOrEven(int num){
+    if(!(num & 1)){
+        cout << "Even\n";
+    } else {
+        cout << "odd\n";
+    }
+}
+
+int main(){
+    oddOrEven(5);
+    oddOrEven(8);
+
+    return 0;
+}
