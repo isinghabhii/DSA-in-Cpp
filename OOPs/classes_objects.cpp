4e7e3b9df85a6694aef29b7cd486d610dab58267
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-08-10 16:54:54 +0530

    classes & objects

--- OOPs/classes_objects.cpp
+++ OOPs/classes_objects.cpp
@@ -2,33 +2,39 @@
 using namespace std;
 
 class Student{
-    //Properties
-    string name;
-    float cgpa;
+    private:
+        //Properties
+        string name;
+    public:
+        float cgpa;
 
-    //Methods
-    void getPercentage(){
-        cout << (cgpa * 10) << "% \n";
-    }
+        //Methods
+        void getPercentage(){
+            cout << (cgpa * 10) << "% \n";
+        }
 };
 
-class User {
-    int id;
-    string username;
-    string password;
-    string bio;
+// class User {
+//     int id;
+//     string username;
+//     string password;
+//     string bio;
 
-    void deactivate(){
-        cout << "deleting account\n";
-    }
+//     void deactivate(){
+//         cout << "deleting account\n";
+//     }
 
-    void editBio(string newBio){
-        bio = newBio;
-    }
-};
+//     void editBio(string newBio){
+//         bio = newBio;
+//     }
+// };
 
 int main(){
     Student s1; //object
-    cout << sizeof(s1);
+    // cout << sizeof(s1);
+    // s1.name = "Abhishek";
+    s1.cgpa = 9.0;
+    cout << s1.cgpa << endl;
+    s1.getPercentage();
     return 0;
 }
