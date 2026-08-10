4a62662781dafb769baaea9ebb5e290253208d69
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-08-10 12:26:58 +0530

    classes_objects

--- OOPs/classes_objects.cpp
+++ OOPs/classes_objects.cpp
@@ -12,7 +12,23 @@ class Student{
     }
 };
 
+class User {
+    int id;
+    string username;
+    string password;
+    string bio;
+
+    void deactivate(){
+        cout << "deleting account\n";
+    }
+
+    void editBio(string newBio){
+        bio = newBio;
+    }
+};
+
 int main(){
     Student s1; //object
+    cout << sizeof(s1);
     return 0;
 }
