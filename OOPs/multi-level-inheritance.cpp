dc80f8cbd54ed8d1ce59cafb5d88bb1d27e1c347
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-08-11 12:51:43 +0530

    multi-level-inheritance

---
+++ OOPs/multi-level-inheritance.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Animal {
+    public:
+        void eat(){
+            cout << "eats\n";
+        }
+        void breathe(){
+            cout << "breathes\n";
+        }
+};
+
+class Mammal : public Animal {
+    public:
+        string bloodType;
+        
+        Mammal(){
+            bloodType = "warm";
+        }
+};
+
+class Dog : public Mammal {
+    public:
+        void tailwag(){
+            cout << "A dog wags its tail\n";
+        }
+};
+
+int main(){
+    Dog d1;
+
+    d1.eat();
+    d1.breathe();
+    d1.tailwag();
+    cout << d1.bloodType << endl;
+
+    return 0;
+}
