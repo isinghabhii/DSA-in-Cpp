81fbd6ef9240ebef1e1bcf5530018d16d5e31f5c
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-08-11 09:53:05 +0530

    inheritance -> single inheritance

---
+++ OOPs/inheritance.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Animal {
+    public:
+        string color;
+
+        void eat(){
+            cout << "eats\n";
+        }
+
+        void breathe(){
+            cout << "breathes\n";
+        }
+};
+
+class Fish : public Animal {
+    public:
+        int fins;
+
+        void swim(){
+            eat();
+            cout << "swims\n";
+        }
+};
+
+int main(){
+    Fish f1;
+    f1.fins = 3;
+    cout << f1.fins << endl;
+    f1.swim();
+    f1.eat();
+    f1.breathe();
+
+    return 0;
+}
