2059d8bfeb0adff9f5cf54eb054907cabbf98826
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-08-13 18:11:20 +0530

    Print X to the power N

---
+++ Recursion/print_X_to_the_power_N.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int pow(int x, int n){ // TC: O(log n)
+    if(n == 0){
+        return 1;
+    }
+
+    int halfPow = pow(x, n/2); 
+    int halfPowSquare = halfPow * halfPow;
+
+    if(n % 2 != 0){
+        //odd
+        return x * halfPowSquare;
+    }
+
+    return halfPowSquare;
+}
+
+int main(){
+    cout << pow(2, 10) << endl;
+
+    return 0;
+}
