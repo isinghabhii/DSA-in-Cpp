cb37435483043df6d9ef8142231d7ad60224069f
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-08-13 17:47:14 +0530

    last occurence

---
+++ Recursion/last_occurence.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int lastOccur(vector<int> arr, int i, int target){
+    if (i == arr.size()){
+        return -1;
+    }
+
+    int idxFound = lastOccur(arr, i+1, target);
+
+    if(idxFound == -1){
+        if(arr[i] == target){
+            return i;
+        }
+    }
+
+    return idxFound;
+}
+
+int main(){
+    vector<int> arr = {1, 2, 3, 3, 3, 4};
+    cout << lastOccur(arr, 0, 3) << endl; // 4
+    return 0;
+} 
