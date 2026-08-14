5c6fdca7867361f95c991da73509241cf25f5e3c
Abhishek Kumar <abhishekkumar33068@gmail.com>
2026-08-14 10:33:17 +0530

    Remove Duplicates in String

--- Recursion/removeDuplicatesInString.cpp
+++ Recursion/removeDuplicatesInString.cpp
@@ -4,10 +4,10 @@
 #include<string>
 using namespace std;
 
-void removeDuplicates(string str, string ans, int i, int map[26]){
+void removeDuplicates(string str, string ans, int i, int map[26]){ // with indices
 
     if(i == str.size()){
-        cout << "ans : " << ans << endl;
+        cout << "ans : " << ans << endl; // ans = apncoleg
         return;
     }
 
@@ -22,11 +22,32 @@ void removeDuplicates(string str, string ans, int i, int map[26]){
     }
 }
 
+void removeDuplicates(string str, string ans, int map[26]){ // without indices 
+
+    if(str.size() == 0){
+        cout << "ans : " << ans << endl; // ans = pnacolge
+        return;
+    }
+
+    int n = str.size();
+    char ch = str[n-1];
+    int mapIdx = (int)(str[n-1] - 'a');
+    str = str.substr(0, n-1);
+
+    if(map[mapIdx]){ //duplicates
+        removeDuplicates(str, ans, map);
+    } else { // not duplicate
+        map[mapIdx] = true;
+        removeDuplicates(str, ch+ans, map);
+    }
+}
+
 int main(){
     string str = "appnnacollege";
     string ans = "";
     int map[26] = {false};
 
-    removeDuplicates(str, ans, 0, map);
+    // removeDuplicates(str, ans, 0, map);
+    removeDuplicates(str, ans, map);
     return 0;
 }
